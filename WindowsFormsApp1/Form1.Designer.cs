669770a951b44eee30409dfd328701355babd8e5
ignaciosironi <ignaciosironi@gmail.com>
2024-04-29 15:54:08 -0300

    se carga proyecto

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox4 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox5 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox6 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox7 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox8 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox9 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox10 = new System.Windows.Forms.CheckedListBox();
+            this.Descomprimir = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.FechaInicio = new System.Windows.Forms.DateTimePicker();
+            this.FechaFin = new System.Windows.Forms.DateTimePicker();
+            this.txtfechainicio = new System.Windows.Forms.TextBox();
+            this.txtfechafin = new System.Windows.Forms.TextBox();
+            this.CalendarFecha = new System.Windows.Forms.MonthCalendar();
+            this.TxtFechaInicioCalendar = new System.Windows.Forms.TextBox();
+            this.TxtFechaFinCalendar = new System.Windows.Forms.TextBox();
+            this.TxtRangoFecha = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(621, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Filtrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(429, 25);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(129, 19);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(621, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Bajar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(621, 456);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Abrir";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(429, 50);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(129, 19);
+            this.checkedListBox2.TabIndex = 6;
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.Location = new System.Drawing.Point(429, 75);
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.Size = new System.Drawing.Size(129, 19);
+            this.checkedListBox3.TabIndex = 7;
+            // 
+            // checkedListBox4
+            // 
+            this.checkedListBox4.FormattingEnabled = true;
+            this.checkedListBox4.Location = new System.Drawing.Point(429, 100);
+            this.checkedListBox4.Name = "checkedListBox4";
+            this.checkedListBox4.Size = new System.Drawing.Size(129, 19);
+            this.checkedListBox4.TabIndex = 8;
+            // 
+            // checkedListBox5
+            // 
+            this.checkedListBox5.FormattingEnabled = true;
+            this.checkedListBox5.Location = new System.Drawing.Point(92, 348);
+            this.checkedListBox5.Name = "checkedListBox5";
+            this.checkedListBox5.Size = new System.Drawing.Size(129, 19);
+            this.checkedListBox5.TabIndex = 9;
+            // 
+            // checkedListBox6
+            // 
+            this.checkedListBox6.FormattingEnabled = true;
+            this.checkedListBox6.Location = new System.Drawing.Point(92, 373);
+            this.checkedListBox6.Name = "checkedListBox6";
+            this.checkedListBox6.Size = new System.Drawing.Size(129, 19);
+            this.checkedListBox6.TabIndex = 10;
+            // 
+            // checkedListBox7
+            // 
+            this.checkedListBox7.FormattingEnabled = true;
+            this.checkedListBox7.Location = new System.Drawing.Point(92, 398);
+            this.checkedListBox7.Name = "checkedListBox7";
+            this.checkedListBox7.Size = new System.Drawing.Size(129, 19);
+            this.checkedListBox7.TabIndex = 11;
+            // 
+            // checkedListBox8
+            // 
+            this.checkedListBox8.FormattingEnabled = true;
+            this.checkedListBox8.Location = new System.Drawing.Point(92, 423);
+            this.checkedListBox8.Name = "checkedListBox8";
+            this.checkedListBox8.Size = new System.Drawing.Size(129, 19);
+            this.checkedListBox8.TabIndex = 12;
+            // 
+            // checkedListBox9
+            // 
+            this.checkedListBox9.FormattingEnabled = true;
+            this.checkedListBox9.Location = new System.Drawing.Point(92, 448);
+            this.checkedListBox9.Name = "checkedListBox9";
+            this.checkedListBox9.Size = new System.Drawing.Size(129, 19);
+            this.checkedListBox9.TabIndex = 13;
+            // 
+            // checkedListBox10
+            // 
+            this.checkedListBox10.FormattingEnabled = true;
+            this.checkedListBox10.Location = new System.Drawing.Point(92, 473);
+            this.checkedListBox10.Name = "checkedListBox10";
+            this.checkedListBox10.Size = new System.Drawing.Size(129, 19);
+            this.checkedListBox10.TabIndex = 14;
+            // 
+            // Descomprimir
+            // 
+            this.Descomprimir.FormattingEnabled = true;
+            this.Descomprimir.Location = new System.Drawing.Point(92, 517);
+            this.Descomprimir.Name = "Descomprimir";
+            this.Descomprimir.Size = new System.Drawing.Size(129, 19);
+            this.Descomprimir.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(334, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Fecha inicio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Fecha fin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(354, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "label4";
+            // 
+            // FechaInicio
+            // 
+            this.FechaInicio.Location = new System.Drawing.Point(92, 23);
+            this.FechaInicio.Name = "FechaInicio";
+            this.FechaInicio.Size = new System.Drawing.Size(200, 20);
+            this.FechaInicio.TabIndex = 22;
+            // 
+            // FechaFin
+            // 
+            this.FechaFin.Location = new System.Drawing.Point(92, 56);
+            this.FechaFin.Name = "FechaFin";
+            this.FechaFin.Size = new System.Drawing.Size(200, 20);
+            this.FechaFin.TabIndex = 23;
+            // 
+            // txtfechainicio
+            // 
+            this.txtfechainicio.Location = new System.Drawing.Point(92, 100);
+            this.txtfechainicio.Name = "txtfechainicio";
+            this.txtfechainicio.Size = new System.Drawing.Size(200, 20);
+            this.txtfechainicio.TabIndex = 24;
+            // 
+            // txtfechafin
+            // 
+            this.txtfechafin.Location = new System.Drawing.Point(92, 135);
+            this.txtfechafin.Name = "txtfechafin";
+            this.txtfechafin.Size = new System.Drawing.Size(200, 20);
+            this.txtfechafin.TabIndex = 25;
+            // 
+            // CalendarFecha
+            // 
+            this.CalendarFecha.Location = new System.Drawing.Point(92, 167);
+            this.CalendarFecha.MaxSelectionCount = 31;
+            this.CalendarFecha.Name = "CalendarFecha";
+            this.CalendarFecha.TabIndex = 26;
+            // 
+            // TxtFechaInicioCalendar
+            // 
+            this.TxtFechaInicioCalendar.Location = new System.Drawing.Point(386, 236);
+            this.TxtFechaInicioCalendar.Name = "TxtFechaInicioCalendar";
+            this.TxtFechaInicioCalendar.Size = new System.Drawing.Size(200, 20);
+            this.TxtFechaInicioCalendar.TabIndex = 27;
+            // 
+            // TxtFechaFinCalendar
+            // 
+            this.TxtFechaFinCalendar.Location = new System.Drawing.Point(386, 279);
+            this.TxtFechaFinCalendar.Name = "TxtFechaFinCalendar";
+            this.TxtFechaFinCalendar.Size = new System.Drawing.Size(200, 20);
+            this.TxtFechaFinCalendar.TabIndex = 28;
+            // 
+            // TxtRangoFecha
+            // 
+            this.TxtRangoFecha.Location = new System.Drawing.Point(386, 150);
+            this.TxtRangoFecha.Name = "TxtRangoFecha";
+            this.TxtRangoFecha.Size = new System.Drawing.Size(200, 20);
+            this.TxtRangoFecha.TabIndex = 29;
+            this.TxtRangoFecha.TextChanged += new System.EventHandler(this.TxtRangoFecha_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 568);
+            this.Controls.Add(this.TxtRangoFecha);
+            this.Controls.Add(this.TxtFechaFinCalendar);
+            this.Controls.Add(this.TxtFechaInicioCalendar);
+            this.Controls.Add(this.CalendarFecha);
+            this.Controls.Add(this.txtfechafin);
+            this.Controls.Add(this.txtfechainicio);
+            this.Controls.Add(this.FechaFin);
+            this.Controls.Add(this.FechaInicio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Descomprimir);
+            this.Controls.Add(this.checkedListBox10);
+            this.Controls.Add(this.checkedListBox9);
+            this.Controls.Add(this.checkedListBox8);
+            this.Controls.Add(this.checkedListBox7);
+            this.Controls.Add(this.checkedListBox6);
+            this.Controls.Add(this.checkedListBox5);
+            this.Controls.Add(this.checkedListBox4);
+            this.Controls.Add(this.checkedListBox3);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+        private System.Windows.Forms.CheckedListBox checkedListBox4;
+        private System.Windows.Forms.CheckedListBox checkedListBox5;
+        private System.Windows.Forms.CheckedListBox checkedListBox6;
+        private System.Windows.Forms.CheckedListBox checkedListBox7;
+        private System.Windows.Forms.CheckedListBox checkedListBox8;
+        private System.Windows.Forms.CheckedListBox checkedListBox9;
+        private System.Windows.Forms.CheckedListBox checkedListBox10;
+        private System.Windows.Forms.CheckedListBox Descomprimir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker FechaInicio;
+        private System.Windows.Forms.DateTimePicker FechaFin;
+        private System.Windows.Forms.TextBox txtfechainicio;
+        private System.Windows.Forms.TextBox txtfechafin;
+        private System.Windows.Forms.MonthCalendar CalendarFecha;
+        private System.Windows.Forms.TextBox TxtFechaInicioCalendar;
+        private System.Windows.Forms.TextBox TxtFechaFinCalendar;
+        private System.Windows.Forms.TextBox TxtRangoFecha;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            /*
+            txtfechainicio.Text=FechaInicio.SelectionRange.ToString
+            txtfechafin.Text = Fechafin.SelectionRange.ToString
+
+
+            txtfechainicio.Text = FechaInicio.SelectionStart.Date
+            txtfechafin.Text=FechaFin.SelectionRange.end
+
+
+            
+
+            TxtRangoFecha.Text=CalendarFecha.SelectionRange.ToString
+
+            TxtFechaInicioCalendar.Text=CalendarFecha.SelectionStart.Date
+
+            TxtFechaFinCalendar.Text=CalendarFecha.SelectionRange.End
+
+            */
+
+            DateTime inicio = CalendarFecha.SelectionStart;
+            DateTime fin = CalendarFecha.SelectionEnd;
+
+            TxtFechaInicioCalendar.Text = inicio.ToString();
+            TxtFechaFinCalendar.Text = fin.ToString();
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtRangoFecha_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
